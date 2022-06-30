0d739bf1500ad411526c4d2d0ca44c4cf36ee9e7
choi <cjh5320@naver.com>
2022-06-30 15:38:02 +0900

    프런트 포함한 javascript 파일

--- app/app.js
+++ app/app.js
@@ -12,6 +12,9 @@ const home = require("./src/routes/home");
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static('./src/public'));
+
+
 
 
 app.use("/",home); //use -> 미들웨어 등록해주는 메서드

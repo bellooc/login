039e01bceb32a2f13491bf55fe5d5338a4dfc943
choi <cjh5320@naver.com>
2022-06-30 16:28:53 +0900

    로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,18 +2,22 @@
 
 // 모듈
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 
 // 라우팅
 const home = require("./src/routes/home");
+const { append } = require("vary");
 
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static('./src/public'));
-
+app.use(bodyParser.json());
+// url을통해 전달되는 데이터에 한글, 공백 등과같은 문자가 포함되 경우 제대로 인식되지않는 문제 해결
+app.use(bodyParser.urlencoded({extended :true } ))
 
 
 
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,15 +1,48 @@
 "use strict";
 
 
-const home = (req, res)=>{
-    res.render("home/index");
-}
 
-const login =  (req,res)=>{
-    res.render("home/login");
-}
+const output ={ 
+    home : (req, res)=>{
+        res.render("home/index");
+    },
+    
+    login :  (req,res)=>{
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["cjh5320","개발"],
+    psword: ["chlwhdghks1~","1234"],
+};
+
+const process = {
+    login : (req,res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+
+        if (users.id.includes(id)) {
+           const idx = users.id.indexOf(id);
+           if(users.psword[idx]===psword) {
+                return res.json({
+                    success:true,
+                })
+
+           }
+        }
+
+        return res.json ({
+            success : false,
+            msg : "로그인 실패",
+        });
+
+    },
+};
+
+
 
 module.exports = {
-    home,
-    login,
+    output,
+    process,
 }
